89845c6d6eff4f061e450b09a9fa1aea293d31a5
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-01-19 18:22:30 +0100

    feat(Mutability): finish Mutability

--- Experiments/Mutability/Mutability.rs
+++ Experiments/Mutability/Mutability.rs
@@ -14,7 +14,7 @@ fn main() {
 
 
     // We're going to create a constant. It can't be mutable, and its type must always be defined.
-    const MINUTES_IN_A_WEEK: u32 = 60 * 24* 7;
+    const MINUTES_IN_A_WEEK: u32 = 60 * 24 * 7;
     /*                        ^
                               |
                               |
@@ -23,13 +23,36 @@ fn main() {
     Constants should be in all uppercase with underscores between words. This makes them easily recognisable.
                         */
 
-    println!("There are {}", MINUTES_IN_A_WEEK)
-
+    println!("There are {} minutes in a week.", MINUTES_IN_A_WEEK);
 
 
     // Shadowing -------------------------------------------------------
-    let y = 5;
-    let y = y + 1;
+    let y = 5.0;
+    let y = y + 1.0;
+    // We introduce a new variable called y. We make its value 5.0.
+    // After that, we recreate that variable, while adding 1.0 to the old one. So it becomes 6.0 .
+    // This is called shadowing. We can use the same variable name multiple times.
 
-    // TODO: https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html#shadowing
+    {
+        let y = y * 0.5;
+        println!("The value of Y in the inner scope is {}", y);
+        // outputs 3. We shadow y once again, but this time, it isn't persistent and returns back to 6.0 outside of the scope.
+    }
+
+    println!("The value of Y in the regular scope is {}", y);
+    // outputs 6. The modification that we did in the scope reverted.
+
+    // Shadowing is different from mutability. We can change the type of the variable with shadowing.
+    let spam = "G423^hu4i23hUY@GYdUG$YzG$uy";
+    println!("Spam: {}.",spam);
+    let spam = spam.len(); // Here, we change the type of spam from &str -> usize. This is possible with shadowing.
+    println!("Spam has {} characters in it.", spam);
+
+
+    /*
+    let mut spam2 = "34h5yt834";
+    spam2 = spam.len(); This won't work, because we can't just change the type of spam2. With shadowing you can, but with mutability you can't.
+     */
+
+
 }
