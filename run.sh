5150c6239e9d02417f0f0709e0db164d973e134e
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-01-19 17:20:22 +0100

    refactor(run.sh)

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-
+#TODO: completions.
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ./run.sh [name]"
-    exit 1
+if [ "$#" -lt 1 ]; then
+  echo "Usage: ./run.sh <name> [-v|--verbose]"
+  exit 1
 fi
+
 
 # Get the name from the command line argument
 name=$1
 
-# Run the experiment with Cargo
-echo "Running $name..."
-RUSTFLAGS=-Awarnings cargo run --bin "$name"
+# If -d flag is provided, run in debug mode
+if [ "$2" == "-v" ] || [ "$2" == "--verbose" ]; then
+  printf -- "-----------\e[93m%s\e[0m-----------\n\n" "$name"
+  cargo run --bin "$name"
+else
+  printf -- "-----------\e[93m%s\e[0m-----------\n\n" "$name"
+  RUSTFLAGS=-Awarnings cargo run -q --release --bin "$name"
+fi
+
