97bdf9cf85ea95849cdde79b408ad2e9223aab9e
Maarten <50550545+mmvanheusden@users.noreply.github.com>
2024-01-19 16:47:22 +0100

    push progress

--- Experiments/Mutability/Mutability.rs
+++ Experiments/Mutability/Mutability.rs
@@ -15,4 +15,21 @@ fn main() {
 
     // We're going to create a constant. It can't be mutable, and its type must always be defined.
     const MINUTES_IN_A_WEEK: u32 = 60 * 24* 7;
+    /*                        ^
+                              |
+                              |
+                        The data type of the constant. This must always be required for constants.
+
+    Constants should be in all uppercase with underscores between words. This makes them easily recognisable.
+                        */
+
+    println!("There are {}", MINUTES_IN_A_WEEK)
+
+
+
+    // Shadowing -------------------------------------------------------
+    let y = 5;
+    let y = y + 1;
+
+    // TODO: https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html#shadowing
 }
