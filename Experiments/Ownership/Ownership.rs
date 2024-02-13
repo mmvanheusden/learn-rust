c65d88207c5a67fddb2f92b81b56f787d59e117c
Maarten <50550545+mmvanheusden@users.noreply.github.com>
2024-02-13 09:06:39 +0100

    feat: add heap/stack explanation

--- Experiments/Ownership/Ownership.rs
+++ Experiments/Ownership/Ownership.rs
@@ -1,5 +1,15 @@
 #![allow(non_snake_case)]
 /// Learning Rusts ownership system.
 fn main() {
-    println!("Hello, world!");
+    /*
+     * The difference between the STACK and the HEAP
+
+     STACK:
+        The stack stores and handles memory a bit like a stack of plates:
+        The last plate that has been added to the pile will also be the first to leave it.
+        Adding data to the stack is called pushing and removing data is called popping.
+        All data on the stack needs to have a fixed size.
+     HEAP:
+        The heap is less organised.
+     */
 }
